e843a34e1820fe240254f7e5db2195d131686cd8
dopi4720 <dopi4720@gmail.com>
2023-03-13 10:12:57 +0700

    Phân quyền

---
+++ DentalManagementSystem/DAL/RolePermissionMapDBContext.cs
@@ -0,0 +1,39 @@
+﻿using DentalManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DentalManagementSystem.DAL
+{
+    public class RolePermissionMapDBContext : DBContext<RolePermissionMap>
+    {
+        public override void Add(RolePermissionMap entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(long Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RolePermissionMap Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<RolePermissionMap> ListAll()
+        {
+            return RolePermissionMaps.Include(x => x.Permission).ToList();
+        }
+
+        public override List<RolePermissionMap> ListAll(long OwnerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(RolePermissionMap entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
