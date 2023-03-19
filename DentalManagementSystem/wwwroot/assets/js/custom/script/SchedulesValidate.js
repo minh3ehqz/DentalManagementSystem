4cccd7f59dcb394a33e4c301d18bcb7c0003f944
minh3ehqz <minh3ehqz@gmail.com>
2023-03-19 21:14:46 +0700

    update

--- DentalManagementSystem/wwwroot/assets/js/custom/script/SchedulesValidate.js
+++ DentalManagementSystem/wwwroot/assets/js/custom/script/SchedulesValidate.js
@@ -26,8 +26,9 @@ var KTSigninGeneralSchedules = function () {
 
                                     var isValidDay = dayOfWeek >= 1 && dayOfWeek <= 5; 
                                     var isValidTime = hours >= 8 && hours < 17; 
-                                    var isValidDate = currentDate <= date && date.getFullYear() - currentDate.getFullYear() <= 1; 
-                                    return isValidDay && isValidTime  && isValidDate;
+                                    var isValidDate = currentDate <= date && date.getFullYear() - currentDate.getFullYear() <= 1;
+                                    var isValidHours = date.getHours() > currentDate.getHours
+                                    return isValidDay && isValidTime && isValidDate && isValidHours;
                                 }
                             },
                         }
